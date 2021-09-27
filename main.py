871ca7c77e69ad3ff8ec3de33cd2894cdaaf8fe5
Your Name <sravanilakshmi.ambati@gmail.com>
2021-09-27 19:51:51 +0530

    digits mapping

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-for x in range(5):
-    for y in range(3):
-        for z in range(1):
-            print(f'({x},{y},{z})')
+phone_number=input('Enter the phone number: ')
+digits_mapping={
+    "0":"Zero",
+    "1":"one",
+    "2":"Two",
+    "3":"Three",
+    "4":"Four",
+    "5":"Five",
+    "6":"Six",
+    "7":"Seven",
+    "8":"Eight",
+    "9":"Nine"
+}
+output=""
+for ch in phone_number:
+    output=output+digits_mapping.get(ch)+" "
+print(output)
---
+++ test.py
@@ -0,0 +1,3 @@
+numbers=[5,3,2,3,5]
+for item in numbers:
+   print('x'* item)
